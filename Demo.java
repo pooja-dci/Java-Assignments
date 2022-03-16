85e49e112bb6bb1d3af35e311c781024f4473915
pooja-dci <36966206+pooja-dci@users.noreply.github.com>
2022-03-15 22:58:11 -0400

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,1 +1,41 @@
+/*
+ * import ArrayList and Scanner from util library
+ */
+public class Demo {
+	
+	//Declare a static ArrayList of Book objects
+	
+	public static void main(String[] args) {
+		
+		//Scanner object to take user input
+		
+		//declare variables needed to store user input information
+		
+		while(true) { //A forever loop. You can also use for(;;)
+			
+			System.out.println("Enter Book title (or 'done' to exit):");
+			//get user input for title
+			//check if title is equal to "done" {
+				//if it is, break; the forever loop
+			//}
+			//get book's author, isbn, rental price, is borrowed values as shown in output example
+			//add an extra .nextLine() after .nextBoolean()
+			
+			//add book object into ArrayList as shown:
+			//books.add(new Book(title, author, isbn, price, isBorrowed));
+			break;
+			
+		}
+		
+		System.out.println("----");
+		System.out.println("The books you entered are:");
+		
+		/*
+		 * Iterate through the ArrayList using one of the ways shown during class.. 
+		 * or use any resource for reference. This assignment is open book
+		 */
+			//print each object inside the ArrayList
+		
+	}
 
+}
